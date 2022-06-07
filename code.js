1883df69ee6c251dd767340be2b23c592078ee0d
mikhail <mikhailthomas29@gmail.com>
2022-06-07 10:26:46 +0200

    .

---
+++ code.js
@@ -0,0 +1,14 @@
+const bestfriends = [
+    'Cameron','Stemmet','Jesse','Spence','Muda','Ssar','King','Mabena','Cassy','Manuel'
+];
+// bestfriends.length = (4);
+
+console.log(bestfriends);
+// console.log(bestfriends [4]);
+
+
+
+console.log(bestfriends[bestfriends.length/2]);
+console.log(bestfriends[bestfriends.length-1]);
+
+
